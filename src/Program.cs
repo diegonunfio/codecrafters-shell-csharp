8f5d98dfa87c2d0a8ab5a8251bc3c7c34a2bf770
Diego Nunfio <dg.nunfio@gmail.com>
2026-08-07 14:38:57 -0600

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,24 +1,35 @@
-class Program
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Write("$ ");
-            // Wait for user input
-            var command = Console.ReadLine();
-            if (command == "exit")
-            {
-                break;
-            }
-            else if (command.StartsWith("echo "))
-            {
-                Console.WriteLine(command[5..]);
-            }
-            else
-            {
-                Console.WriteLine($"{command}: command not found");
-            }
+class Program {
+  static void Main(string[] args) {
+
+    while (true) {
+      var exit = "exit";
+      var echo = "echo";
+      var type = "type";
+      var valid = new[] { exit, echo, type };
+
+      Console.Write("$ ");
+      var Test = Console.ReadLine();
+      if (valid.Any(Test.Contains)) {
+        if (Test == "exit") {
+          return;
+        } else if (Test.StartsWith("echo")) {
+          Console.WriteLine(Test[5..]);
+        } else if (Test.Contains("exit")) {
+          Console.WriteLine($"{Test[5..]} is a shell builtin");
+        } else if (Test.Contains("echo")) {
+          Console.WriteLine($"{Test[5..]} is a shell builtin");
         }
+
+        else if (Test.EndsWith("type")) {
+          Console.WriteLine($"{Test[5..]} is a shell builtin");
+
+        } else {
+          Console.WriteLine($"{Test[5..]}: not found");
+        }
+
+      } else {
+        Console.WriteLine($"{Test}: command not found");
+      }
     }
+  }
 }
